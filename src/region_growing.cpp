4697a9b272555624fd1198aff75971c27b283415
sysadm <sysadm@imesbv01.(none)>
2014-07-03 13:25:15 +0200

    integrated VTK and failed calculation of the BoundingBox

--- src/region_growing.cpp
+++ src/region_growing.cpp
@@ -171,71 +171,71 @@ int main(int argc, char** argv)
                 Matrix4f p2w (Matrix4f::Identity());
                 p2w.block<3,3>(0,0 = eigDx.transpose)
 
-//
-//                //calc boundingbox
-//              pcl::PCA<pcl::PointXYZ> pca;
-//                pcl::PointCloud<pcl::PointXYZ> proj;
-//
-//                pca.setInputCloud (planes_projected);
-//                pca.project (*planes_projected, proj);
-//
-//                pcl::PointXYZ proj_min;
-//                pcl::PointXYZ proj_max;
-//                pcl::getMinMax3D (proj, proj_min, proj_max);
-//
-//                pcl::PointXYZ min;
-//                pcl::PointXYZ max;
-//                pca.reconstruct (proj_min, min);
-//                pca.reconstruct (proj_max, max);
-//                std::cout << " min.x= " << min.x << " max.x= " << max.x << " min.y= " <<
-//                             min.y << " max.y= " << max.y << " min.z= " << min.z << " max.z= " << max.z
-//                          << std::endl;
-//
-//                //Rotation of PCA
-//                Eigen::Matrix3f rot_mat = pca.getEigenVectors ();
-//
-//                //translation of PCA
-//                Eigen::Vector3f cl_translation = pca.getMean().head(3);
-//
-//                Eigen::Matrix3f affine_trans;
-//                std::cout << rot_mat << std::endl;
-//                //Reordering of principal components
-//                affine_trans.col(0) <<
-//                                       (rot_mat.col(0).cross(rot_mat.col(1))).normalized();
-//                affine_trans.col(1) << rot_mat.col(0);
-//                affine_trans.col(2) << rot_mat.col(1);
-//                //affine_trans.col(3) << cl_translation,1;/**/
-//
-//                std::cout << affine_trans << std::endl;
-//
-//                Eigen::Quaternionf rotation = Eigen::Quaternionf (affine_trans);
-//                Eigen::Vector4f t = pca.getMean();
-//
-//                Eigen::Vector3f translation = Eigen::Vector3f (t.x(), t.y(), t.z());
-//
-//                double width = fabs(proj_max.x-proj_min.x);
-//                double height = fabs(proj_max.y-proj_min.y);
-//                double depth = fabs(proj_max.z-proj_min.z);
-//
-//                //adding the bounding box to a viewer :
-//                boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new
-//                pcl::visualization::PCLVisualizer ("3D Viewer"));
-//                viewer->setBackgroundColor (0, 0, 0);
-//                viewer->addPointCloud<pcl::PointXYZ> (planes_projected, "NAO arm cloud");
-//                viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "NAO arm cloud");
-//
-//                viewer->addCoordinateSystem ();
-//                viewer->initCameraParameters ();
-//                //viewer->addCube (min.x, max.x, min.y, max.y, min.z, max.z);/**/
-//                viewer->addCube (translation, rotation, width, height, depth);
-//                while (!viewer->wasStopped ())
-//                {
-//                    viewer->spinOnce (100);
-//                    boost::this_thread::sleep (boost::posix_time::microseconds (100000));
-//                }
-//                //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-//
-                *planes_cloud+=*planes_projected;//Alle Clusterebenen, die vertikal sind werden in planes_cloud gespeichert
+                        //
+                        //                //calc boundingbox
+                        //              pcl::PCA<pcl::PointXYZ> pca;
+                        //                pcl::PointCloud<pcl::PointXYZ> proj;
+                        //
+                        //                pca.setInputCloud (planes_projected);
+                        //                pca.project (*planes_projected, proj);
+                        //
+                        //                pcl::PointXYZ proj_min;
+                        //                pcl::PointXYZ proj_max;
+                        //                pcl::getMinMax3D (proj, proj_min, proj_max);
+                        //
+                        //                pcl::PointXYZ min;
+                        //                pcl::PointXYZ max;
+                        //                pca.reconstruct (proj_min, min);
+                        //                pca.reconstruct (proj_max, max);
+                        //                std::cout << " min.x= " << min.x << " max.x= " << max.x << " min.y= " <<
+                        //                             min.y << " max.y= " << max.y << " min.z= " << min.z << " max.z= " << max.z
+                        //                          << std::endl;
+                        //
+                        //                //Rotation of PCA
+                        //                Eigen::Matrix3f rot_mat = pca.getEigenVectors ();
+                        //
+                        //                //translation of PCA
+                        //                Eigen::Vector3f cl_translation = pca.getMean().head(3);
+                        //
+                        //                Eigen::Matrix3f affine_trans;
+                        //                std::cout << rot_mat << std::endl;
+                        //                //Reordering of principal components
+                        //                affine_trans.col(0) <<
+                        //                                       (rot_mat.col(0).cross(rot_mat.col(1))).normalized();
+                        //                affine_trans.col(1) << rot_mat.col(0);
+                        //                affine_trans.col(2) << rot_mat.col(1);
+                        //                //affine_trans.col(3) << cl_translation,1;/**/
+                        //
+                        //                std::cout << affine_trans << std::endl;
+                        //
+                        //                Eigen::Quaternionf rotation = Eigen::Quaternionf (affine_trans);
+                        //                Eigen::Vector4f t = pca.getMean();
+                        //
+                        //                Eigen::Vector3f translation = Eigen::Vector3f (t.x(), t.y(), t.z());
+                        //
+                        //                double width = fabs(proj_max.x-proj_min.x);
+                        //                double height = fabs(proj_max.y-proj_min.y);
+                        //                double depth = fabs(proj_max.z-proj_min.z);
+                        //
+                        //                //adding the bounding box to a viewer :
+                        //                boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new
+                        //                pcl::visualization::PCLVisualizer ("3D Viewer"));
+                        //                viewer->setBackgroundColor (0, 0, 0);
+                        //                viewer->addPointCloud<pcl::PointXYZ> (planes_projected, "NAO arm cloud");
+                        //                viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "NAO arm cloud");
+                        //
+                        //                viewer->addCoordinateSystem ();
+                        //                viewer->initCameraParameters ();
+                        //                //viewer->addCube (min.x, max.x, min.y, max.y, min.z, max.z);/**/
+                        //                viewer->addCube (translation, rotation, width, height, depth);
+                        //                while (!viewer->wasStopped ())
+                        //                {
+                        //                    viewer->spinOnce (100);
+                        //                    boost::this_thread::sleep (boost::posix_time::microseconds (100000));
+                        //                }
+                        //                //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                        //
+                        *planes_cloud+=*planes_projected;//Alle Clusterebenen, die vertikal sind werden in planes_cloud gespeichert
                 //std::stringstream ss;
                 //ss<<"Cluster_"<<a<<".pcd";
                 //writer.write<pcl::PointXYZ>(ss.str(),*cluster_cloud,false);
