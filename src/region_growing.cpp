cb79facd8a3c8be995a95b7b8fec6bc7619ebfed
sysadm <sysadm@imesbv01.(none)>
2014-07-03 11:35:18 +0200

    integrated VTK and failed calculation of the BoundingBox

--- src/region_growing.cpp
+++ src/region_growing.cpp
@@ -26,6 +26,16 @@
 #include <pcl/io/vtk_io.h>                  // Surface/Mesh als VTK abspeichern
 #include <pcl/io/vtk_lib_io.h>              // Mesh als STL abspeichern
 
+#include <vtkUnstructuredGrid.h>
+#include <vtkPoints.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkSmartPointer.h>
+
+
 int main(int argc, char** argv)
 {
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_unfiltered (new pcl::PointCloud<pcl::PointXYZ>);
@@ -51,7 +61,7 @@ int main(int argc, char** argv)
     normal_estimator.setSearchMethod(tree);
     normal_estimator.setInputCloud(cloud);
     normal_estimator.setKSearch(100);// Ursprünglich:50
-                                    // http://pointclouds.org/documentation/tutorials/normal_estimation.php
+    // http://pointclouds.org/documentation/tutorials/normal_estimation.php
     normal_estimator.compute(*normals);
 
     pcl::RegionGrowing<pcl::PointXYZ, pcl::Normal> reg;
@@ -59,12 +69,12 @@ int main(int argc, char** argv)
     //reg.setMaxClusterSize (10000);
     reg.setSearchMethod (tree);
     reg.setNumberOfNeighbours (50); // Ursprünglich 50 (Bei ca. 150 werden die Kanten schön glatt!)
-                                    // aber es wird nach zu vielen Nachbarn gecheckt-> Mehrere Banden fallen in ein Cluster
+    // aber es wird nach zu vielen Nachbarn gecheckt-> Mehrere Banden fallen in ein Cluster
     reg.setInputCloud (cloud);
     //reg.setIndices (indices);
     reg.setInputNormals (normals);
     reg.setSmoothnessThreshold (4.0 / 180.0 * M_PI);    // Ursprünglich: 7.0/180*M_PI
-                                                        // je kleiner desto weniger punkte sind "smooth" genug ->Klares Abgrenzen der Cluster!
+    // je kleiner desto weniger punkte sind "smooth" genug ->Klares Abgrenzen der Cluster!
     reg.setCurvatureThreshold (.4);//Ursprünglich:1.0   // je kleiner desto geringer darf sich das Cluster krümmen
 
     // Anwendung des Cluster-Filters auf Input-Wolke "cloud"
@@ -75,7 +85,7 @@ int main(int argc, char** argv)
     extract.setInputCloud(cloud);
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr planes_cloud (new pcl::PointCloud<pcl::PointXYZ>);
-/*NUR FÜR STATIV!!!
+    /*NUR FÜR STATIV!!!
     //Suchen des größten Clusters ->Bodenebene ->Berechnung der Ebene
     //Dieser Schritt ist nur bis zur Extraktion und Speicherung der Bodenebene geschrieben,
     // da durch Anpassung der Number of Neigbors alle vertikalen Ebenen bereits erkannt wurden
@@ -104,66 +114,126 @@ int main(int argc, char** argv)
     //(In Cluster_cloud ist immer nur ein Cluster!)//////
     int b=0;
     for(int a=0;a<clusters.size();a++)
-{
-    if(clusters[a].indices.size() >50 )
     {
-    pcl::IndicesPtr indices_ptr (new std::vector<int> (clusters[a].indices.size ()));
-    for (int i=0;i<indices_ptr->size();i++)
-    {
-        (*indices_ptr)[i]=clusters[a].indices[i];//http://www.pcl-users.org/Removing-a-cluster-Problem-with-pointer-td4023699.html
+        if(clusters[a].indices.size() >50 )
+        {
+            pcl::IndicesPtr indices_ptr (new std::vector<int> (clusters[a].indices.size ()));
+            for (int i=0;i<indices_ptr->size();i++)
+            {
+                (*indices_ptr)[i]=clusters[a].indices[i];//http://www.pcl-users.org/Removing-a-cluster-Problem-with-pointer-td4023699.html
+            }
+
+            // Punkte des Clusters werden in cluster_cloud geschrieben
+            extract.setIndices(indices_ptr);
+            extract.setNegative(false);
+            extract.filter(*cluster_cloud);// Punkte des Cluster a werden in cluster_cloud geschrieben
+            std::cout<<"cluster_cloud["<<a<<"] hat "<<cluster_cloud->width*cluster_cloud->height<<" Punkte."<<std::endl;
+
+            //Erzeugen einer Ebene aus Cluster_cloud
+            pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
+            pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
+            pcl::SACSegmentation<pcl::PointXYZ> seg;
+            seg.setOptimizeCoefficients(true);
+            seg.setModelType(pcl::SACMODEL_PLANE);
+            seg.setMethodType(pcl::SAC_RANSAC);
+            seg.setMaxIterations(1000);
+            seg.setDistanceThreshold(0.1);
+            seg.setInputCloud(cluster_cloud);
+            seg.segment(*inliers, *coefficients);
+
+            // Wenn Ebene vertikal: Abspeichern in Cluster_i.pcd
+            if(coefficients->values[2]<.9 && coefficients->values[2]>(-.9))//ax+by+cz+d=0 (wenn c=0 => Ebene parallel zur z-Achse)
+            {
+                pcl::PointCloud<pcl::PointXYZ>::Ptr planes_projected (new pcl::PointCloud<pcl::PointXYZ>);
+                //Inliers auf Ebene projizieren:
+                pcl::ProjectInliers<pcl::PointXYZ> proj;
+                proj.setModelType(pcl::SACMODEL_PLANE);
+                proj.setIndices(inliers);
+                proj.setInputCloud(cluster_cloud);
+                proj.setModelCoefficients(coefficients);
+                proj.filter(*planes_projected);
+
+                //Convert to vtkPointSet to get BoundingBox
+                vtkSmartPointer< vtkUnstructuredGrid > vtkPs = vtkSmartPointer<vtkUnstructuredGrid >::New();
+                vtkSmartPointer< vtkPoints > vtkPts = vtkSmartPointer<vtkPoints>::New();
+
+                for(int z = 0; z< planes_projected->size(); ++z)
+                    vtkPts->InsertNextPoint(planes_projected->points[z].x, planes_projected->points[z].y, planes_projected->points[z].z);
+
+                vtkPs->SetPoints(vtkPts);
+
+                //Debug **********************************************************************************************************
+
+                vtkPs->ComputeBounds();
+                double* bb = vtkPs->GetBounds();
+
+                pcl::PointCloud <pcl::PointXYZRGB>::Ptr boundingBox(new pcl::PointCloud<pcl::PointXYZRGB> );
+                pcl::PointXYZRGB pt(255,0,0);
+                pt.x= bb[0];
+                pt.y= bb[1];
+                pt.z= bb[2];
+                boundingBox->push_back(pt);
+                pt.x= bb[3];
+                pt.y= bb[1];
+                pt.z= bb[2];
+                boundingBox->push_back(pt);
+                pt.x= bb[0];
+                pt.y= bb[1];
+                pt.z= bb[5];
+                boundingBox->push_back(pt);
+                pt.x= bb[3];
+                pt.y= bb[1];
+                pt.z= bb[5];
+                boundingBox->push_back(pt);
+                pt.x= bb[0];
+                pt.y= bb[4];
+                pt.z= bb[2];
+                boundingBox->push_back(pt);
+                pt.x= bb[3];
+                pt.y= bb[4];
+                pt.z= bb[2];
+                boundingBox->push_back(pt);
+                pt.x= bb[0];
+                pt.y= bb[4];
+                pt.z= bb[5];
+                boundingBox->push_back(pt);
+                pt.x= bb[3];
+                pt.y= bb[4];
+                pt.z= bb[5];
+                boundingBox->push_back(pt);
+
+
+
+                pcl::visualization::CloudViewer viewer ("Bounding Box viewer");
+
+                viewer.showCloud(planes_projected);
+                viewer.showCloud(boundingBox);
+                while (!viewer.wasStopped ())
+                {
+                }
+                //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+                *planes_cloud+=*planes_projected;//Alle Clusterebenen, die vertikal sind werden in planes_cloud gespeichert
+                //std::stringstream ss;
+                //ss<<"Cluster_"<<a<<".pcd";
+                //writer.write<pcl::PointXYZ>(ss.str(),*cluster_cloud,false);
+                //std::cout<<"Cluster "<<a<<" wurde gespeichert!"<<std::endl;
+                b=b+1;
+            }
+        }
     }
-
-    // Punkte des Clusters werden in cluster_cloud geschrieben
-    extract.setIndices(indices_ptr);
-    extract.setNegative(false);
-    extract.filter(*cluster_cloud);// Punkte des Cluster a werden in cluster_cloud geschrieben
-    std::cout<<"cluster_cloud["<<a<<"] hat "<<cluster_cloud->width*cluster_cloud->height<<" Punkte."<<std::endl;
-
-    //Erzeugen einer Ebene aus Cluster_cloud
-    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
-    pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
-    pcl::SACSegmentation<pcl::PointXYZ> seg;
-    seg.setOptimizeCoefficients(true);
-    seg.setModelType(pcl::SACMODEL_PLANE);
-    seg.setMethodType(pcl::SAC_RANSAC);
-    seg.setMaxIterations(1000);
-    seg.setDistanceThreshold(0.1);
-    seg.setInputCloud(cluster_cloud);
-    seg.segment(*inliers, *coefficients);
-
-    // Wenn Ebene vertikal: Abspeichern in Cluster_i.pcd
-    if(coefficients->values[2]<.9 && coefficients->values[2]>(-.9))//ax+by+cz+d=0 (wenn c=0 => Ebene parallel zur z-Achse)
-    {
-        pcl::PointCloud<pcl::PointXYZ>::Ptr planes_projected (new pcl::PointCloud<pcl::PointXYZ>);
-        //Inliers auf Ebene projizieren:
-        pcl::ProjectInliers<pcl::PointXYZ> proj;
-        proj.setModelType(pcl::SACMODEL_PLANE);
-        proj.setIndices(inliers);
-        proj.setInputCloud(cluster_cloud);
-        proj.setModelCoefficients(coefficients);
-        proj.filter(*planes_projected);
-
-    *planes_cloud+=*planes_projected;//Alle Clusterebenen, die vertikal sind werden in planes_cloud gespeichert
-    //std::stringstream ss;
-    //ss<<"Cluster_"<<a<<".pcd";
-    //writer.write<pcl::PointXYZ>(ss.str(),*cluster_cloud,false);
-    //std::cout<<"Cluster "<<a<<" wurde gespeichert!"<<std::endl;
-        b=b+1;
-    }
-    }
-}
     std::cout<<"Es wurden "<<b<<" Flächen in Planes_cloud.pcd geschrieben"<<endl;
     writer.write("Planes_cloud.pcd",*planes_cloud,false);
 
     std::cout << "Number of clusters is equal to " << clusters.size () << std::endl;
     std::cout << "First cluster has " << clusters[0].indices.size () << " points." << endl;
     std::cout << "These are the indices of the points of the initial" <<
-      std::endl << "cloud that belong to the first cluster:" << std::endl;
+                 std::endl << "cloud that belong to the first cluster:" << std::endl;
     int counter = 0;
     while (counter < 5 || counter > clusters[0].indices.size ())
     {
-      std::cout << clusters[0].indices[counter] << std::endl;
-      counter++;
+        std::cout << clusters[0].indices[counter] << std::endl;
+        counter++;
     }
 
     //TEST: planes_cloud zu Surface konvertieren u. als stl oder vtk speichern!
@@ -203,7 +273,7 @@ int main(int argc, char** argv)
     pcl::io::savePolygonFileSTL("mesh.stl", triangles);
     //pcl::io::saveVTKFile("mesh.vtk", triangles);
 
-            //hier weitermachen!!!
+    //hier weitermachen!!!
 
 
     pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud = reg.getColoredCloud ();
@@ -213,7 +283,7 @@ int main(int argc, char** argv)
     {
     }
 
-/*
+    /*
     pcl::PointCloud <pcl::PointXYZ>::Ptr cluster_cloud=clusters[0].indices;
     pcl::visualization::CloudViewer viewer ("Cluster viewer");
     viewer.showCloud(cluster_cloud);
@@ -221,6 +291,6 @@ int main(int argc, char** argv)
     {
     }
 */
-      return (0);
+    return (0);
 
 }
